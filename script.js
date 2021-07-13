50c40b81acf958ebf51a28f9154dedb0c53f5970
catherinestirk <catherine.stirk@tipigroup.com>
2021-07-13 15:28:43 +0100

    add cookies script

--- script.js
+++ script.js
@@ -33,3 +33,15 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = "We use cookies for improved functionality and analytics";
+message.innerHTML = "We use cookies for improved functionality and analytics <button class='btn btn--close-cookie'> Got it! </button>";
+header.prepend(message);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove();
+})
