54df1a2f9e075c9d1e30589e58732624a16bc695
catherinestirk <catherine.stirk@tipigroup.com>
2021-09-07 10:42:43 +0100

    add styles to cookie message

--- script.js
+++ script.js
@@ -40,8 +40,11 @@ const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.textContent = "We use cookies for improved functionality and analytics";
 message.innerHTML = "We use cookies for improved functionality and analytics <button class='btn btn--close-cookie'> Got it! </button>";
-header.prepend(message);
+header.append(message);
 
 document.querySelector('.btn--close-cookie').addEventListener('click', function(){
   message.remove();
 })
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
