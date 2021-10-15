78f1e0c142aa968c8d24702d7f19854fff49ebc4
catherinestirk <catherine.stirk@tipigroup.com>
2021-10-15 14:46:15 +0100

    add lazy loading to images

--- script.js
+++ script.js
@@ -166,4 +166,27 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 allSections.forEach(function(section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
-})
+});
+
+//lazy loading images 
+const imgTargets = document.querySelectorAll('img[data-src');
+
+const loadImg = function(entries, observer){
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+
+  //replace src attribute with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root:null,
+  threshold: 0
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
